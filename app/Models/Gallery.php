20860c914a3bb851df05a6bf7dc4a388539a2776
farras55 <farraskuyy@gmail.com>
2025-12-15 17:46:30 +0700

    add home gallery

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -20,4 +20,22 @@ class Gallery extends Model
             ->bind(':type', $type)
             ->fetchAll();
     }
+
+    public function countByType($type)
+    {
+        return $this->db
+            ->query("SELECT COUNT(id) as total FROM {$this->table} WHERE type = :type")
+            ->bind(':type', $type)
+            ->fetch()->total;
+    }
+
+    public function getByTypePaginated($type, $limit, $offset)
+    {
+        return $this->db
+            ->query("SELECT * FROM {$this->table} WHERE type = :type ORDER BY upload_date DESC LIMIT :limit OFFSET :offset")
+            ->bind(':type', $type)
+            ->bind(':limit', (int) $limit, \PDO::PARAM_INT)
+            ->bind(':offset', (int) $offset, \PDO::PARAM_INT)
+            ->fetchAll();
+    }
 }
